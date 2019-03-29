c6478c5eaaa737f19461882e984329b37527b1cd
RicardoTurco <ricturco@gmail.com>
2019-03-29 09:20:54 -0300

    after lesson 9 - FORMULARIOS

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,12 +1,18 @@
 from flask import render_template
 from app import app
 
+from app.models.forms import LoginForm
+
 
 @app.route("/index/<user>")
 @app.route("/", defaults={"user":None})
 def index(user):
     return render_template('index.html', user=user)
 
-@app.route("/login")
+@app.route("/login", methods=["POST"])
 def login():
-    return render_template('index.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    return render_template('login.html', form=form)
